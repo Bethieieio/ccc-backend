a1a193aac02474a9c75a1d95faa4c6f272e24886
Beth Allaway <beth_allaway@yahoo.com>
2023-08-01 18:40:28 +0100

    add api auth paths

--- ccc_backend/ccc_backend/urls.py
+++ ccc_backend/ccc_backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -39,5 +39,11 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    path('api-auth', include('rest_framework.urls')),
+
+    path('', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration', include('dj_rest_auth.registration.urls')
+        ),
 
 ]
