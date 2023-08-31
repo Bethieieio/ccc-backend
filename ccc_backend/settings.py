6ad745144ca629fd0c88fba258ba6667d5edd85a
Beth Allaway <beth_allaway@yahoo.com>
2023-08-31 20:46:16 +0100

    added whitenoise for swagger

--- ccc_backend/settings.py
+++ ccc_backend/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
@@ -178,3 +179,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- ccc_backend/wsgi.py
+++ ccc_backend/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ccc_backend.settings')
 
-application = get_wsgi_application()
+wsgiApp = get_wsgi_application()
+
+application = WhiteNoise(wsgiApp, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
